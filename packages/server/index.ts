0cad6705762f23095489cd7d158aaca2ae67685f
Biplob Talukdar <biplob.codes@gmail.com>
2026-06-11 01:25:18 +0600

    Send random quote

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import { prisma } from './lib/prisma';
 const app = express();
 app.get('/', (req: Request, res: Response) => {
    res.send('Hello World!!!');
 });
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello from the Server!' });
+app.get('/api/quote', async (req: Request, res: Response) => {
+   const count = await prisma.quote.count();
+   const random = Math.floor(Math.random() * count);
+
+   const quote = await prisma.quote.findFirst({
+      skip: random,
+      take: 1,
+      include: { author: true },
+   });
+   res.json(quote);
 });
 
 const port = process.env.PORT || '3000';
